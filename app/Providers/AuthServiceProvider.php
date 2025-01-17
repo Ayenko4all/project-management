27694debe187cafaac7b8e2dcf9b1f67cc62d2f7
Ayeni kehinde Oluwatosin <kenny4all40@gmail.com>
2025-01-17 15:20:23 +0100

    clean up

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -2,44 +2,11 @@
 
 namespace App\Providers;
 
-use App\Models\Account;
-use App\Models\Beneficiary;
-use App\Models\Card;
-use App\Models\CardPayment;
-use App\Models\CardRequest;
-use App\Models\Investment;
-use App\Models\ManagerUser;
-use App\Models\Messages;
-use App\Models\Permission;
-use App\Models\Pos;
-use App\Models\PosPayment;
+
 use App\Models\Project;
-use App\Models\RecurringDebitMandate;
-use App\Models\Role;
-use App\Models\Token;
-use App\Models\TransactionEntry;
-use App\Models\User;
-use App\Options\DefaultRoles;
-use App\Policies\AccountPolicy;
-use App\Policies\BeneficiaryPolicy;
-use App\Policies\CardPaymentPolicy;
-use App\Policies\CardRequestPolicy;
-use App\Policies\CardsPolicy;
-use App\Policies\InvestmentPolicy;
-use App\Policies\ManagerPolicy;
-use App\Policies\MessagePolicy;
-use App\Policies\PermissionPolicy;
-use App\Policies\PosPaymentPolicy;
-use App\Policies\PosPolicy;
 use App\Policies\ProjectPolicy;
-use App\Policies\RecurringDebitMandatePolicy;
-use App\Policies\RolePolicy;
-use App\Policies\TokenPolicy;
-use App\Policies\TransactionEntryPolicy;
-use App\Policies\UserPolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Illuminate\Support\Facades\Gate;
-use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -49,7 +16,7 @@ class AuthServiceProvider extends ServiceProvider
      * @var array
      */
     protected $policies = [
-        Project::class           => ProjectPolicy::class,
+        Project::class => ProjectPolicy::class,
     ];
 
     /**
